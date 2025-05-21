0c5aa66f69d46c7a34b7c9ccf87de165e3028496
Aswathi-6 <aswathimurugesan2406@gmail.com>
2025-05-21 21:20:41 +0530

    Add files via upload

---
+++ Solution14.java
@@ -0,0 +1,8 @@
+class Solution14 {
+    public boolean rotateString(String s, String goal) {
+     if(s.length()!=goal.length())
+     return false;
+     String newString=s+s;
+     return newString.contains(goal);   
+    }
+}
